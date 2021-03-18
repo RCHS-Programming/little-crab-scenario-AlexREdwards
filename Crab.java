491332972e6570710abc152b7fb721cd1458c296
AlexREdwards <alexander.edwards@stu.boone.kyschools.us>
2021-03-18 08:40:45 -0400

    key move and sound

--- Crab.java
+++ Crab.java
@@ -8,11 +8,23 @@ public class Crab extends Actor
     public void act()
     {
        
-       
+       checkKeyPress();
        move();
        LookForWorm();
-       randomTurn();
-       turnAtEdge();
+       
+       
+    }
+    
+    public void checkKeyPress()
+    {
+        if (Greenfoot.isKeyDown("left"))
+       {
+           turn (-4);
+       }
+        if (Greenfoot.isKeyDown("right"))
+        {
+            turn (4);
+        }
     }
     
     public void move( )
@@ -28,25 +40,14 @@ public class Crab extends Actor
     {
         if ( isTouching (Worm.class) )        
        {
-           removeTouching (Worm.class);        
+           removeTouching (Worm.class);     
+           Greenfoot.playSound ("slurp.wav");
        }
     }
     
-    public void randomTurn()
-    {
-        if ( Greenfoot.getRandomNumber(100) < 10 )                 
-       {
-           turn( Greenfoot.getRandomNumber(91) - 45);                
-       }
-    }
     
-    public void turnAtEdge()
-    {
-        if ( isAtEdge() ) 
-       {
-           turn(17);
-       }
-    }
+    
+   
 }
 
 
